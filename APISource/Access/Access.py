79dcea82462135ace6b1bb6dae2f966dfbd707e7
İhsan DEMİRCİ <141773094+DoritosLover1@users.noreply.github.com>
2024-08-29 01:17:27 +0300

    Update Access.py

--- APISource/Access/Access.py
+++ APISource/Access/Access.py
@@ -10,7 +10,6 @@ class Access:
             self.url = "https://real-time-amazon-data.p.rapidapi.com/search"
             self.commons_parameters = {"query": query, "country": country, "sort_by": sort,
                                    "product_condition": p_condition, "is_prime": "false"}
-            # Burada aslında query, page, country, sort_by, product_condition hepsi parametre olarak girecez.
             self.querystring = {}
 
             self.headers = {
