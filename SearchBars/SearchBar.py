a4cf5501e3e54dd8b2d1226cce1a60e7d4041b8a
İhsan DEMİRCİ <141773094+DoritosLover1@users.noreply.github.com>
2024-08-29 01:11:57 +0300

    Update SearchBar.py

--- SearchBars/SearchBar.py
+++ SearchBars/SearchBar.py
@@ -5,20 +5,20 @@ from kivy.graphics import Color, RoundedRectangle
 class SearchBar(FloatLayout):
     def __init__(self, **kwargs):
         super(SearchBar, self).__init__(**kwargs)
-        # Yuvarlatılmış köşeli arka plan
+
         with self.canvas.before:
-            Color(0.9, 0.9, 0.9, 1)  # Hafif gri arka plan
+            Color(0.9, 0.9, 0.9, 1) 
             self.background = RoundedRectangle(
                 pos=self.pos,
                 size=self.size,
-                radius=[15]  # Köşe yarıçapı
+                radius=[15]
             )
         self.information = TextInput(
             size_hint=(1, 1),
             pos_hint={'center_x': 0.5, 'center_y': 0.5},
             hint_text="Enter what you wanna search",
             multiline=False,
-            background_color=(0,0,0,0)  # Şeffaf arka plan
+            background_color=(0,0,0,0)
         )
         self.add_widget(self.information)
         self.bind(size=self.update_background, pos=self.update_background)
